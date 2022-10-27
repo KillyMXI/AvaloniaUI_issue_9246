23c4d9e15bcd6664e20ed592355aa4095b24334f
KillyMXI <killy.mxi@gmail.com>
2022-10-27 23:54:31 +0400

    first commit

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,9 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvaloniaApplication1.ViewModels;
+public class ViewModelBase : ReactiveObject
+{
+}
